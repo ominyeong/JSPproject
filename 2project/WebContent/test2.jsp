6b1dfd3f3de54ef6a591087242519865b64126ac
301 <301@223.194.169.94>
2015-03-20 16:42:35 +0900

    rrrrrr

--- 2project/WebContent/test2.jsp
+++ 2project/WebContent/test2.jsp
@@ -1,5 +1,15 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
+<%
+
+	out.print("Hello JSP"); // 내장객채 = OUT
+	
+	String name = request.getParameter("name");
+	
+	out.print(name);
+
+%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,12 +18,12 @@
 </head>
 <body>
 
-<h1>GET</h1>
-<form method="get" action="HelloServlet">
-		이름 : <input type="text" name="name"><br>
-		아이디 : <input type="text" name="id"><br>
+<h1>GET</h1> 
+<form method="get" action="HelloServlet">   
+		이름 : <input type="text" name="name"><br>  
+		아이디 : <input type="text" name="id"><br>  
 		비밀번호 : <input type="password" name="pwd"><br>
-	<input type="submit" value="GET 방식 요청">
+	<input type="submit" value="GET 방식 요청"> 
 </form>
 
 <!-- HTML 주석 -->
@@ -32,7 +42,7 @@
 <hr>
 
 <h1>JSP</h1>
-<form action="test3.jsp">
+<form method="get" action="test3.jsp"></form>
 		이름 : <input type="text" name="name"><br>
 		아이디 : <input type="text" name="id"><br>
 		비밀번호 : <input type="password" name="pwd"><br>
--- 2project/src/exam/first/HelloServlet.java
+++ 2project/src/exam/first/HelloServlet.java
@@ -12,14 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet("/HelloServlet") //url mapping이라고 한다
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**	
      * @see HttpServlet#HttpServlet()
      */
-    public HelloServlet() {
+    public HelloServlet() {  //이 생성자는 클라이언트에서 서버쪽으로 리퀘스트를 했을때 자동으로 생성이된다
         super();
         // TODO Auto-generated constructor stub
     }
@@ -36,12 +36,12 @@ public class HelloServlet extends HttpServlet {
 		
 		PrintWriter out = response.getWriter();
 		
-		String a = request.getParameter("name");
+		String name = request.getParameter("name");  //네임이라는 값이 a에 대입된다.
 		String i = request.getParameter("id");
 		
 		out.print("Hello - GET<br>");
-		out.print("이름 : " + a + "<br>");
-		out.print(i);
+		out.print("이름 : " + name  + "<br>");   //위에 a이듯 여기도a 라고 써야함
+		out.print(i);						    //위에 i이듯 여기도i 라고 써야함
 	}
 
 	/**
@@ -49,17 +49,17 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		// 한글 처리
 		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		
 		PrintWriter out = response.getWriter();
 		
-		String b = request.getParameter("name");
+		String name = request.getParameter("name");
 		String c = request.getParameter("id");
-		
+		//out.print("<html>");
 		out.print("Hello - POST<br>");
-		out.print("이름 : " + b + "<br>");
+		out.print("이름 : " + name + "<br>");
 		out.print(c);
 	}
 
